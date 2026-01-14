9486e638bc1fb00fcc9f4d296725fa0fbb5fb8b0
Andreas Hall (VCC) <39693569+andreas-cae@users.noreply.github.com>
2026-01-14 15:42:43 +0100

    3. Security groups - Create a security group
    - Add a security group for web server
    - Use default VPC and get its information using the 'data' command

--- main.tf
+++ main.tf
@@ -14,11 +14,56 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true                # Constrain to be the default VPC
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = ["aws_security_group.blog.id"]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_security_group" "blog" {
+  # Name will show of in AWS console, if not def, name will be random generated
+  name        = blog
+  description = "Allow HTTP(s) in allow all out"
+  
+  vpc_id = data.aws_vpc.default.id  # set ID is the default vpc
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # All incomming IP
+  
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # All incomming IP
+  
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]  # All incomming IP
+  
+  security_group_id = aws_security_group.blog.id
+}
+
