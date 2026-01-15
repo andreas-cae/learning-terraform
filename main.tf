13a8a3cf32e494b1a2f5d915a2239fc6018a3fc9
Andreas Hall (VCC) <39693569+andreas-cae@users.noreply.github.com>
2026-01-15 13:44:13 +0100

    3. TF registry modules - Remove old security group
    Cleanup - removed old security group, only keeping the new module based.

--- main.tf
+++ main.tf
@@ -33,8 +33,9 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
-  name    = "blog_new"
+  name    = "blog"
   use_name_prefix = false
+  description = "SG for webserver created using the security_group module"
 
   vpc_id        = data.aws_vpc.default.id  # set ID is the default vpc
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
@@ -43,41 +44,4 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "blog" {
-  # Name will show of in AWS console, if not def, name will be random generated
-  name        = "blog"
-  description = "Allow HTTP(s) in allow all out"
-  
-  vpc_id = data.aws_vpc.default.id  # set ID is the default vpc
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]  # All incomming IP
-  
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]  # All incomming IP
-  
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]  # All incomming IP
-  
-  security_group_id = aws_security_group.blog.id
-}
 
