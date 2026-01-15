cccd46dc44ab4d1330f2d6f2a4c7bd0901c0e28b
Andreas Hall (VCC) <39693569+andreas-cae@users.noreply.github.com>
2026-01-15 15:21:06 +0100

    4. Expanding your installation - Add http loadbalacer

--- main.tf
+++ main.tf
@@ -50,6 +50,60 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  
+  name    = "blog-alb"                      # WAS "my-alb"
+
+  vpc_id  = module.blog_vpc.vpc_id          # WAS "vpc-abcde012"
+  subnets = module.blog_vpc.public_subnets  # WAS ["subnet-abcde012", "subnet-bcde012a"]
+  security_groups = module.blog_sg.security_group_id
+
+  # access_logs = {
+  #   bucket = "my-alb-logs"
+  # }
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+        # my_other_target = {
+        #  target_id = "i-a1b2c3d4e5f6g7h8i"
+        #  port = 8080
+        # }
+      }
+    }
+  ]
+
+  # https_listeners = [
+  #   {
+  #     port               = 443
+  #     protocol           = "HTTPS"
+  #     certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+  #     target_group_index = 0
+  #   }
+  # ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source          = "terraform-aws-modules/security-group/aws"
   version         = "5.3.1"
