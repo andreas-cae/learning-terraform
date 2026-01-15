5d7cceac07743377cf7d5ecdf9fc4b99357f83f3
Andreas Hall (VCC) <39693569+andreas-cae@users.noreply.github.com>
2026-01-15 14:23:30 +0100

    4. Get Ready to scale - Create vpc using VPC module

--- main.tf
+++ main.tf
@@ -18,12 +18,32 @@ data "aws_vpc" "default" {
   default = true                # Constrain to be the default VPC
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_instance" "blog" {
+  ami                   = data.aws_ami.app_ami.id
+  instance_type         = var.instance_type
   # vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -31,17 +51,18 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.1"
-  name    = "blog"
+  source          = "terraform-aws-modules/security-group/aws"
+  version         = "5.3.1"
+  name            = "blog"
   use_name_prefix = false
-  description = "SG for webserver created using the security_group module"
+  description     = "SG for webserver created using the security_group module"
 
-  vpc_id        = data.aws_vpc.default.id  # set ID is the default vpc
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  # vpc_id        = data.aws_vpc.default.id  # set ID is the default vpc
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules  = ["all-all"] 
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"] 
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 
