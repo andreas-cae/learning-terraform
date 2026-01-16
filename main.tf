5450e9bf5173f641feed92869c3d1ef6c7382c64
Andreas Hall (VCC) <39693569+andreas-cae@users.noreply.github.com>
2026-01-16 18:56:11 +0100

    bugfix - Remove target from ALB

--- main.tf
+++ main.tf
@@ -87,11 +87,11 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
+#      targets = {
+#        my_target = {
+#          target_id = aws_instance.blog.id
+#          port = 80
+#        }
         # my_other_target = {
         #  target_id = "i-a1b2c3d4e5f6g7h8i"
         #  port = 8080
