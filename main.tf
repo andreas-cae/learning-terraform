d29a05ad7626efbdf662e426a6465ead5ba27287
Hall, Andreas <andreas.hall@volvocars.com>
2026-02-20 15:05:42 +0100

    Revert to 5d7ccea, 4. Get Ready to scale - Create vpc using VPC module

--- main.tf
+++ main.tf
@@ -37,88 +37,16 @@ module "blog_vpc" {
   }
 }
 
-# resource "aws_instance" "blog" {
-#  ami                   = data.aws_ami.app_ami.id
-#   instance_type         = var.instance_type
-#   # vpc_security_group_ids = [aws_security_group.blog.id]
-#   vpc_security_group_ids = [module.blog_sg.security_group_id]
-# 
-#   subnet_id = module.blog_vpc.public_subnets[0]
-# 
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
+resource "aws_instance" "blog" {
+  ami                   = data.aws_ami.app_ami.id
+  instance_type         = var.instance_type
+  # vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-
-  name     = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets    # Specify subnets!
-  target_groups_arns  = module.blog_alb.target_group_arns # arns=amzon resource numbers
-  # the target group is created by the loadbalancer
-  security_groups     = [ module.blog_sg.security_group_id ]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-}
-
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-    
-  name    = "blog-alb"                      # WAS "my-alb"
-
-  vpc_id  = module.blog_vpc.vpc_id          # WAS "vpc-abcde012"
-  subnets = module.blog_vpc.public_subnets  # WAS ["subnet-abcde012", "subnet-bcde012a"]
-  security_groups = [ module.blog_sg.security_group_id ]
-
-  # access_logs = {
-  #   bucket = "my-alb-logs"
-  # }
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-        # my_other_target = {
-        #  target_id = "i-a1b2c3d4e5f6g7h8i"
-        #  port = 8080
-        # }
-      }
-    }
-  ]
-
-  # https_listeners = [
-  #   {
-  #     port               = 443
-  #     protocol           = "HTTPS"
-  #     certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-  #     target_group_index = 0
-  #   }
-  # ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
-    Environment = "dev"
+    Name = "HelloWorld"
   }
 }
 
